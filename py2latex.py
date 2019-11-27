160e89dcc4fbe8d65492c5df05a6caa3501a5e05
KarlNaumann <karl.naumann@icloud.com>
2019-11-27 21:04:51 +0000

    v1.0.0
    Basic method for generating and exporting tables. Requires documentation and debugging.

--- py2latex.py
+++ py2latex.py
@@ -7,67 +7,52 @@ import pandas as pd
 class latexTable:
     '''Class to control the threeparttable'''
     
-    def __init__(self,df,title,label,index=False):
-        self.title = title
-        self.label = df.index.tolist()
-        self.rows,self.cols = df.shape
-        self.index = index
-        self.df = df
+    def __init__(self,df,title,label,columns=None,index=True,ixTitle=None,precision=3,threeparttable=False):
+        self.output=[]
+        self._makeHeader(index,title,label,df.shape[1])
+        if not columns: columns = df.columns
+        self._makeColHeader(columns)
+        self._fillTable(self,df,index,precision)
     
-    def _beginTable(self):
-        self.output = []
+    def _makeHeader(self,index,title,label,cols):
         self.output.append('\\begin{table}[H]')
         self.output.append('\\centering')
         self.output.append('\\caption{%s} \\label{%s}'%(title,label))
-        r = ''.join(['r' for x in range(self.cols)])
-        if self.index: r = 'l'+r
+        r = ''.join(['r' for x in cols])
+        if index: r = 'l'+r
         self.output.append('\\begin{tabular}{%s}'%(r))
         
-    def _header(self,header=False,indexTitle=False):
-        if header:
-            header = ''.join(['{%s & }'%col for col in header])
+    def _makeColHeader(self, columns,ixTitle):
+        """Adds the column headings - can be specified extra
+        requires: column headings"""
+        self.output.append('\\toprule')
+        if columns:
+            header = ''.join(['{%s & }'%col for col in columns])
         else:
             header = ''.join(['{%s & }'%col for col in self.df.columns])
-        if indexTitle: header = '{%s} &'%indexTitle + header
+        if ixTitle: header = '{%s} &'%indexTitle + header + '\\\\'
         self.output.append(header)
-
-        def latex_table(df,title,label,indexTitle='',save=True,precision=None,index=True,verbose=False):
-            output=[]
-            #Basic beginning structure of the LaTeX matrix
-            output.append('\\begin{table}[H]')
-            output.append('\\centering')
-            output.append('\\caption{%s} \\label{%s}'%(title,label[:-4]))
-            #Formatting the alignment of the LaTeX matrix
-            rows,cols = df.shape
-            r = ''.join(['r' for x in range(len(df.columns))])
-            if index: r = 'l'+r
-            output.append('\\begin{tabular}{%s}'%(r))
-            #Header row
-            header=''
-            if index: header+='{%s} &'%indexTitle
-            for col in df.columns:
-                header += '%s &'%col
-            #Delete the last '&' and put '\\'
-            output.append(header[:-2]+'\\\\')
-            output.append('\\midrule')
-            #Body of the table
-            for i in range(rows):
-                newrow =''
-                if index: newrow+='\\textbf{%s} & '%df.index[i]
-                for j in range(cols):
-                    val = df.iloc[i,j]
-                    if precision is not None: val = round(val, precision)
-                    newrow+='{} & '.format(val) 
-                output.append(newrow[:-2] + '\\\\')
-            #End of table format
-            output.append('\\bottomrule')
-            output.append('\\end{tabular}')  
-            output.append('\\end{table}') 
-            #write the table to a file
-            if save:
-                with open(label,'w') as f:
-                    f.write('\n'.join(output))
-                    f.close()
-            if verbose: print('\n'.join(output),'\n')
-            #return string of the latex output
-            return output
+        self.output.append('\\midrule')
+    
+    def _fillTable(self,df,index,precision):
+        """Fill the table with data from the dataframe"""
+        for i in range(df.shape[0]):
+            newrow =''
+            if index: newrow+='\\textbf{%s} & '%df.index[i]
+            for j in range(df.shape[1]):
+                val = df.iloc[i,j]
+                if precision is not None: val = round(val, precision)
+                newrow+='{} & '.format(val) 
+            self.output.append(newrow[:-2] + '\\\\')
+        #End of table format
+        self.output.append('\\bottomrule')
+        self.output.append('\\end{tabular}')  
+        self.output.append('\\end{table}')
+        
+    def export(label):
+        """Export dataframe as latex file to use easily. 
+        Require file name, e.g. 'tab:results' with or with .tex to end"""
+        if label[-4:]!='.tex':label+'.tex'
+        with open(label,'w') as f:
+            f.write('\n'.join(self.output))
+            f.close()
